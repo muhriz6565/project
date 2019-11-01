263657540a3a9af462230d81e72b8cfd458e6213
muhriz6565 <56537183+muhriz6565@users.noreply.github.com>
2019-11-01 10:34:38 +0000

    Add files via upload

---
+++ project-master2/choice.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import urllib.request,time,csv,datetime,pytz
+import releaseschedulescraper, new_release_magscrape
+#print("Done(?)")
+choice=input('Enter anime name: ')
+choice=choice.lower()
+with open('schedule.csv', 'r', newline='') as csvfile:
+    spamwriter=csv.reader(csvfile, delimiter=',')
+    for row in spamwriter:
+        title=row[0].replace(" ","")
+        if  choice.replace(" ","") ==title.lower() :
+            name=row[0]
+            day=row[1]
+            showtime=row[2]
+            break
+with open('data.csv', 'r', newline='') as csvfile:
+    spamwriter=csv.reader(csvfile, delimiter=',')
+    for row in spamwriter:
+        if name == row[1]:
+            link="https://horriblesubs.info"+row[2]
+            showid=row[0]
+            print("FOUND") #CALL ANOTHER SCRIPT HERE "new_release_magscrape.py"
+            new_release_magscrape.latest_scrape(showid)
+            break
+        
+######datetime.datetime.now(pytz.timezone("US/Pacific"))
+current_time=str(datetime.datetime.now(pytz.timezone("US/Pacific")))[11:16]
+while True:
+    if current_time==showtime:
+        print("grab the mag links")
+        break
+    current_time=str(datetime.datetime.now(pytz.timezone("US/Pacific")))[11:16]
+    
+print(current_time)
---
+++ project-master2/releaseschedulescraper.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import urllib.request,time,csv
+req=urllib.request.Request(url="https://horriblesubs.info/release-schedule/",
+                           headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+soup = BeautifulSoup((urllib.request.urlopen(req)), features="html.parser")
+noodles=(soup.find_all("a",title="See all releases for this show"))
+scrapedtimes=soup.find_all(class_="schedule-time")
+##days=soup.find_all(class_="weekday")
+titles=[]
+times=[]
+join=[]
+daycounter=0
+weekdays=["Monday",'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+##for i in days:
+##    print((i.text)[0:9])
+for i in noodles:
+    titles.append(i.text)
+actual_times=[]
+for i in range(0,len(scrapedtimes)):
+    times.append(weekdays[daycounter])
+    actual_times.append(scrapedtimes[i].text)
+    if i!=len(scrapedtimes)-1:
+        if int((scrapedtimes[i].text)[:1])>int((scrapedtimes[i+1].text)[:1]):
+            daycounter+=1    
+for i in range(0,len(titles)):
+    join.append([titles[i],times[i],actual_times[i]])
+#print(join)
+
+with open('schedule.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',')
+    spamwriter.writerow(["showname","day","time"])
+    for i in join:
+        spamwriter.writerow(i)
+
+
+#return choice
